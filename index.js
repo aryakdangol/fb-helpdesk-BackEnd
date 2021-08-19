ed22c7d9c66e066976c9d51dba35f04d93fdd257
Aryak Dangol <aryak.dangol9@gmail.com>
2021-08-20 03:54:09 +0545

    Added MongoDB Atlas

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const commentRouter = require("./routes/commentmessage");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const URL = process.env.MONGO_URL;
+console.log(URL);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
@@ -25,10 +28,6 @@ app.use("/userComments", commentRouter);
 app.listen(process.env.PORT || 5000);
 
 mongoose
-  .connect(
-    "mongodb://localhost/fb-helpdesk",
-    { useNewUrlParser: true },
-    { useUnifiedTopology: true }
-  )
+  .connect(URL, { useNewUrlParser: true }, { useUnifiedTopology: true })
   .then(console.log("DATABASE CONNECTED SUCCESSFULLY"))
   .catch((e) => console.log(e));
